8dd41d4a1bb0738229d1a13fd21ea8a33111d646
Christos Tsagkournis <Christos.Tsagkournis@sgdigital.com>
2021-04-11 14:30:18 +0300

    Minor change in the application properties naming

--- data-rest/src/main/java/gr/nothingness/backofficeusermanager/configuration/ApplicationProperties.java
+++ data-rest/src/main/java/gr/nothingness/backofficeusermanager/configuration/ApplicationProperties.java
@@ -6,7 +6,7 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@ConfigurationProperties(prefix = "app")
+@ConfigurationProperties(prefix = "application")
 public class ApplicationProperties {
 
   @Getter @Setter private String basePath = "/";
